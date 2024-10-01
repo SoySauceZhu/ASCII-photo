3c1b9134e32af0cb97e64b72b270d04b46a19f18
Soy_SauceZhu <zhumingjie0627@outlook.com>
2024-10-01 13:31:57 +0100

    Update processBase.py

--- processBase.py
+++ processBase.py
@@ -1,8 +1,9 @@
 """
-The abstract base class of
-mosaic (dimensional reduction) processor
+The abstract base class of image procrssor
+Takes in original frame image, convert to grayscale,
+maps to ascii array and returen the ascii scheme
 
-Dimensional reduction image processor should
+Image processor should
 inherit from this interface
 and implement api in the class
 
@@ -19,6 +20,10 @@ class ProcessBase(ABC):
         """
         Constructor takes in a tuple of (width, height),
         defining the output image shape as class attributes
+
+        reduced_resolution: int, the height of output ascii scheme (reduced image)
+        chars: string, chars from high density to low
+        aspect_ratio: tuple, i.e. (16, 9)
         """
         self.reduced_rows = reduced_resolution
         self.reduced_cols = reduced_resolution * aspect_ratio[0] // aspect_ratio[1]
@@ -28,9 +33,10 @@ class ProcessBase(ABC):
     @abstractmethod
     def ascii_array(self, input_image) -> np.ndarray:
         """
-        Return the image (array) of reduced image 
+        Takes in the original frame image
+        Return the ascii scheme (np.array) of reduced image 
 
-        input: origin frame image from webcam (grayscale -- i.e. [1080, 1920])
-        output: reduced image (array) 
+        input: origin frame image from webcam (i.e. [1080, 1920, 3])
+        output: ascii scheme (array) 
         """
         pass
